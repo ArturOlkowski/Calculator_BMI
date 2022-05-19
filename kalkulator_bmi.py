9a4f0843a3738ebfd3cbf09c27915e28ea642303
Artur Olkowski <artur.olkowski90@gmail.com>
2022-05-19 16:32:04 +0200

    Add new file

---
+++ kalkulator_bmi.py
@@ -0,0 +1,27 @@
+weight = float(input("What is you current weight? :"))
+height = float(input("What is your height? :"))
+
+
+def count(weight,height):
+
+    BMI = weight/(height ** 2)
+
+    if BMI < 18.5:
+            print("You are underweight")
+    elif BMI > 19 and BMI < 24.9:
+        print("You'r weight in correnct!")
+    elif BMI > 25 and BMI < 29.9:
+        print("You are overweight")
+    elif BMI > 30:
+        print("You'r obes")
+    else:
+        print("weight out of range!")
+       
+    print("You'r BMI score is:",round(BMI,2))
+
+    print(BMI)
+    
+count(weight,height)
+
+
+
